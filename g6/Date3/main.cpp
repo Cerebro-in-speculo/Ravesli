b0a628757bbb6e810cafaae5325cfeafc250a30f
LedoRomus <r.ledovskih@gmail.com>
2019-03-26 00:10:13 +0300

    release v1

---
+++ g6/Date3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int array[] = { 7, 5, 6, 4, 9, 8, 2, 1, 3 };
+
+    int scoreDate=sizeof(array)/sizeof(array[0]);
+
+    for(int i=0;i<scoreDate;++i)
+        std::cout<<array[i]<<" ";
+    std::cout<<'\n';
+
+    return 0;
+}
