12d9cd69fc3a0afc893f5aa0587b4edc20dfe7d5
R.Ledovskih <R.Ledovskih@gmail.com>
2019-04-03 08:21:02 +0300

    v2

--- g6/NEW2/hmain.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_MAIN
-#define H_MAIN
-
-void getName(std::string *,int);
-void sortName(std::string *,int);
-void setName(std::string*,int);
-
-#endif
---
+++ g6/NEW3/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include"hmain.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout<<"Hawe name need enter?: ";
+    int lenght{0};
+    std::cin>>lenght;
+
+    std::cin.ignore(32678,'\n');
+
+    std::cout<<"Enter this name:\n";
+    std::string *name=new std::string [lenght];
+
+    getName(name,lenght);
+    sortName(name,lenght);
+    setName(name,lenght);
+
+    delete[]name;
+	name = nullptr;
+
+	system("pause");
+
+    return 0;
+}
